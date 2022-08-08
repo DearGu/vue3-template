ed9fa68432c1d2033f9eaa901b1765c8cc7aa780
DearGu <137471594@qq.com>
2022-08-08 16:08:47 +0800

    stylint less相关

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
+
+console.log(666);
 </script>
 
 <template>
@@ -16,14 +18,14 @@ import HelloWorld from './components/HelloWorld.vue';
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  &:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
